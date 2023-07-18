f0ffdff5a6b66dea45633629f372c72d097c539f
alex903802 <67serat@gmail.com>
2023-07-18 12:20:41 +0300

    edit

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import vk_api
+from sqlalchemy import create_engine
 from vk_api.longpoll import VkLongPoll, VkEventType
 from vk_api.utils import get_random_id
 
-from database import Base, add_profile, check_profile
-from sqlalchemy import create_engine
-from config import  access_token, token_group, db_url_object
 from bot import Vkinder
+from config import access_token, token_group, db_url_object
+from database import Base, add_profile, check_profile
+
 
 class Vkinderinterface():
     def __init__(self, token_group, access_token):
@@ -24,6 +25,36 @@ class Vkinderinterface():
                         'random_id': get_random_id()}
                        )
 
+    def data_age(self, user_id):
+        self.message_send(user_id, 'Введите возраст: ')
+        message, user_id = self.event_handler()
+        age_prof = str(message)
+        age_user = self.vk_bot.params['age']
+        for age_info in age_user:
+            if age_info['age'] == age_prof.title():
+                self.message_send(user_id, f'Поиск по возрасту {age_prof.title()} лет')
+                return age_prof.title()
+
+    def data_sex(self, user_id):
+        self.message_send(user_id, 'Введите пол: ')
+        message, user_id = self.event_handler()
+        sex_prof = str(message)
+        sex_user = self.vk_bot.params['sex']
+        for sex_info in sex_user:
+            if sex_info['sex'] == sex_prof.title():
+                self.message_send(user_id, f'Поиск противоположного пола {sex_prof.title()}')
+                return sex_prof.title()
+
+    def data_city(self, user_id):
+        self.message_send(user_id, 'Введите пол: ')
+        message, user_id = self.event_handler()
+        city_prof = str(message)
+        city_user = self.vk_bot.params['city']
+        for city_info in city_user:
+            if city_info['title'] == city_prof.title():
+                self.message_send(user_id, f'Поиск по введному названия города {sex_prof.title()}')
+                return city_prof.title()
+
     def event_handler(self):
         for event in self.longpoll.listen():
             if event.type == VkEventType.MESSAGE_NEW and event.to_me:
@@ -33,17 +64,17 @@ class Vkinderinterface():
                         self.message_send(
                             event.user_id, f'Привет, {self.params["name"]}!\n'
                                            f'Для продолжения  укажи свой город')
-                        event.to_me = self.params['city']
+                        event.to_me = self.data_city(event.user_id)
                     elif self.params['age'] is None:
                         self.message_send(
                         event.user_id, f'Привет, {self.params["name"]}!\n'
                         f'Укажите свой возраст')
-                        event.to_me = self.params['age']
+                        event.to_me = self.data_age(event.user_id)
                     elif self.params['sex'] is None:
                         self.message_send(
                         event.user_id, f'Привет, {self.params["name"]}!\n'
                         f'Укажите свой пол')
-                        event.to_me = self.params['sex']
+                        event.to_me = self.data_sex(event.user_id)
                     else:
                         self.message_send(event.user_id, f'Привет {self.params["name"]}')
                 elif event.text.lower() == 'поиск':
@@ -70,7 +101,7 @@ class Vkinderinterface():
                                 all_users.add(str(tool['id']))
                                 num_users += 1
 
-                                if check_profile(engine, str(tool['id'])):
+                                if check_profile(engine, tool):
                                     self.message_send(event.user_id, f"{tool['name']} уже есть в базе данных.")
                                     continue
                         photos = self.vk_bot.get_photo(tool["id"])
